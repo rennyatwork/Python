4fe4e0c3491a9aa600237cec18fa46ed42a8255b
Renato <rennyatwork@gmail.com>
2018-05-09 19:41:42 -0400

    techbeamers.com/python-tutorial-step-by-step

--- Hello/hello.py
+++ Hello/hello.py
@@ -1,5 +1,20 @@
 msg ="Hello world!"
 print(msg)
 msg.capitalize()
-print("msg.capitalize(): ", msg.capitalize())
+# print("msg.capitalize(): ", msg.capitalize())
 print("oi")
+
+print "5 % 2 = ", 5%2
+
+nameList = ["Joe", "Clyde", "Isaac"]
+
+print (nameList)
+print (nameList[0])
+
+print ('Interest calculator')
+amount = float(input('Principal amount?'))
+roi = float(input('Rate of interest?'))
+years = int(input('Duration (years)?' ))
+total = (amount* pow(1+ (roi/100), years))
+interest = total - amount
+print('\n Interest = %0.2f' %interest)
